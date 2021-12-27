6483a12182eb68a37adee082fa219ca98d5fd4cb
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2021-12-27 06:21:23 +0100

    Correctly waiting for 30secs after movement detected
    GOD DAMMIT WHY DID IT TAKE SO LONG

--- wled00/usermod_v2_pirSensor.h
+++ wled00/usermod_v2_pirSensor.h
@@ -2,73 +2,162 @@
 
 #include "wled.h"
 
-uint16_t presetWhenMovementDetected = 1; // labeled as "Save to ID" in webinterface
-uint16_t presetWhenNoMovementDetected = 2;
-
 class UsermodPirSensor : public Usermod
 {
 private:
   uint32_t previousCheckTime;
   uint32_t currentCheckTime;
-  bool previousPirSensorState;
-  bool currentPirSensorState;
 
-  // constants
-  const uint8_t PirSensorPin = 13;         // D7 on hardware
-  const uint32_t m_switchOffDelay = 60000; // 1 min delay before switch off after the sensor state goes LOW
-  const uint16_t checkFrequencyMs = 1000;  // how often to check sensor in milliseconds
+  bool currentPinState;
+
+  bool keepMovementFlag;
+  bool checkAnywaysFlag;
+
+  uint16_t keepMovementCounter; // counter to keep HIGH/MOVED status for a x time after movement was detected
+
+  uint16_t currentPreset;
+  uint16_t previousPreset;
+
+  // *********** CONSTANTS ***********
+  const uint8_t pirSensorPin = 16;               // D0 on hardware
+  const uint32_t keepMovementDelaySeconds = 30;  // 1 min delay before switch off after the sensor state goes LOW
+  const uint16_t checkFrequencyMs = 1000;        // how often to check sensor in milliseconds, if you want "keepMovementDelaySeconds" to make sense this has to stay at 1000ms
+  const uint16_t presetWhenMovementDetected = 1; // labeled as "Save to ID" in webinterface
+  const uint16_t presetWhenNoMovementDetected = 2;
+
+  // debug var
+  // int resultingPreset;
 
 public:
   UsermodPirSensor() : previousCheckTime(0),
                        currentCheckTime(0),
-                       previousPirSensorState(0),
-                       currentPirSensorState(0)
+                       currentPinState(LOW),
+                       keepMovementFlag(false),
+                       checkAnywaysFlag(false),
+                       keepMovementCounter(0),
+                       currentPreset(0),
+                       previousPreset(0)
   {
   }
   void setup()
   {
     // Serial.println("Hello from usermod pir sensor!");
-    pinMode(PirSensorPin, INPUT);
+    pinMode(pirSensorPin, INPUT);
   }
 
   void connected()
   {
-    Serial.println("WIFI CONNECTED!");
+    // Serial.println("WIFI CONNECTED!");
   }
 
   void loop()
+  {
+    checkPirSensorPeriodically();
+  }
+
+  // check pir sensor state and apply preset
+  void checkPirSensorPeriodically()
   {
     currentCheckTime = millis();
     if (currentCheckTime - previousCheckTime > checkFrequencyMs)
     {
       previousCheckTime = currentCheckTime;
-      // Serial.println("Checking sensor!");
-      checkPirSensor();
+      // run state machine every x milliseconds
+      checkSensorState();
     }
-  }
-
-  // check pir sensor state and apply preset
-  void checkPirSensor()
-  {
-    currentPirSensorState = digitalRead(PirSensorPin);
-    if (currentPirSensorState != previousPirSensorState)
+    else
     {
-      // Serial.println("Makes sense to apply preset!");
-      // only set preset when it needed == when pirSensorState changes
-      if (currentPirSensorState)
+      // no need to check, not enough time passed
+    }
+  }
+
+  void checkSensorState()
+  {
+    currentPinState = digitalRead(pirSensorPin);
+
+    if (checkAnywaysFlag && keepMovementFlag == false)
+    {
+      checkAnywaysFlag = false;
+      // change on pin detected, and i'm not told to keep state
+      if (currentPinState == HIGH)
       {
-        // Serial.println("presetWhen MovementDetected");
-        applyPreset(presetWhenMovementDetected);
+        keepMovementFlag = true; // set flag to wait with going back to non movement state
+        keepMovementCounter = 0; // reset counter
+        applyPresetIfNeeded(presetWhenMovementDetected);
+        // resultingPreset = 1;
       }
-      else
+      else if (currentPinState == LOW)
       {
-        // Serial.println("presetWhen NoMovementDetected");
-        applyPreset(presetWhenNoMovementDetected);
+        applyPresetIfNeeded(presetWhenNoMovementDetected);
+        // resultingPreset = 0;
       }
     }
 
-    previousPirSensorState = currentPirSensorState;
+    // keep restarting the counter if pin state is kept HIGH, so 30sec without movement -> no movement preset
+    if (currentPinState == HIGH)
+      keepMovementCounter = 0;
+
+    // do the counting for keeping movement state
+    if (keepMovementCounter < keepMovementDelaySeconds && keepMovementFlag)
+    {
+      keepMovementCounter++;
+    }
+    else
+    {
+      // time passed, can turn off flag now
+      keepMovementFlag = false;
+      keepMovementCounter = 0;
+      // special flag to make it check pin even with no change detected
+      checkAnywaysFlag = true;
+    }
+    // Debug print
+    // Serial.printf("pinState=%i\tkeepMovementCounter=%i\tkeepMovementFlag=%i\toutput(0-no mov)=%i\n", currentPinState, keepMovementCounter, keepMovementFlag, resultingPreset);
   }
 
-  void applyCorrectPreset();
+  void applyPresetIfNeeded(uint16_t preset)
+  {
+    currentPreset = preset;
+    if (currentPreset != previousPreset)
+    {
+      // Serial.printf("!!!!!! ACTUALLY CHANGED PRESET NOW, PRESET CHANGED TO:%i !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n", currentPreset);
+      applyPreset(currentPreset);
+    }
+    previousPreset = currentPreset;
+  }
+
+  /*
+   * addToConfig() can be used to add custom persistent settings to the cfg.json file in the "um" (usermod) object.
+   * It will be called by WLED when settings are actually saved (for example, LED settings are saved)
+   * If you want to force saving the current state, use serializeConfig() in your loop().
+   *
+   * CAUTION: serializeConfig() will initiate a filesystem write operation.
+   * It might cause the LEDs to stutter and will cause flash wear if called too often.
+   * Use it sparingly and always in the loop, never in network callbacks!
+   *
+   * addToConfig() will also not yet add your setting to one of the settings pages automatically.
+   * To make that work you still have to add the setting to the HTML, xml.cpp and set.cpp manually.
+   *
+   * I highly recommend checking out the basics of ArduinoJson serialization and deserialization in order to use custom settings!
+   */
+  void addToConfig(JsonObject &root)
+  {
+    // Serial.println("selectedPirSetting saved");
+
+    // JsonObject top = root.createNestedObject("UsermodPirSensor");
+    // top["selectedPirSetting"] = false; //save this var persistently whenever settings are saved
+  }
+
+  /*
+   * readFromConfig() can be used to read back the custom settings you added with addToConfig().
+   * This is called by WLED when settings are loaded (currently this only happens once immediately after boot)
+   *
+   * readFromConfig() is called BEFORE setup(). This means you can use your persistent values in setup() (e.g. pin assignments, buffer sizes),
+   * but also that if you want to write persistent values to a dynamic buffer, you'd need to allocate it here instead of in setup.
+   * If you don't know what that is, don't fret. It most likely doesn't affect your use case :)
+   */
+  void readFromConfig(JsonObject &root)
+  {
+    // JsonObject top = root["top"];
+    // userVar0 = top["great"] | 42; //The value right of the pipe "|" is the default value in case your setting was not present in cfg.json (e.g. first boot)
+  }
 };
